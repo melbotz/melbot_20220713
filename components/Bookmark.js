d1a368f3fadf63d36f60730a295b8d34a2b61006
Melvin Carvalho <https://melvincarvalho.com/#me>
2022-07-13 15:27:52 +0200

    better description

--- components/Bookmark.js
+++ components/Bookmark.js
@@ -2,16 +2,25 @@
 // IMPORTS
 import { render, html } from '../js/preacthtm.js'
 
+// FUNCTIONS
+function getTextFromUri(uri) {
+  var source = 'reddit'
+  var sub = uri.split('/')[4]
+  return `${sub} - ${source}`
+}
+
 // COMPONENTS
 export default function Bookmark(props) {
   let uri = props.uri || '#'
   let title = props.title || 'bookmark'
   let image = props.image || '#'
+
+  let text = getTextFromUri(uri)
   return html`
           <td>
             <img height="200" src="${image}" /> ${'\n'}
             <br />
-            <a style="color: blue" href="${uri}" target="_blank">reddit 🔗</a>
+            <a style="color: blue" href="${uri}" target="_blank">${text} 🔗</a>
           </td>
         `
 }
